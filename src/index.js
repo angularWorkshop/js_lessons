0e6f33c90c25bc9d3129b06fb33d9328c30fb4b5
Denis Svirko <denis.svirko@users.noreply.github.com>
2026-04-06 22:53:30 +0300

    feat: add bound notifier starter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export function hello() {
-  return 'js_lessons baseline';
+'use strict';
+
+export function createNotifier(prefix) {
+  return {
+    prefix,
+    format(message) {
+      return this.prefix + ": " + message;
+    },
+  };
+}
+
+export function createSavedFormatter(notifier) {
+  // TODO: return a callback that will keep notifier as its context later
+  void notifier;
+}
+
+export function runLater(callback, message) {
+  return callback(message);
 }
