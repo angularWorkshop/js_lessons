1c74b64a4fe791678cb5841879369060d45cb7b7
Denis Svirko <denis.svirko@users.noreply.github.com>
2026-04-06 23:02:05 +0300

    feat: add id generator solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export function hello() {
-  return 'js_lessons baseline';
+'use strict';
+
+export function* createIdGenerator(prefix, start = 1) {
+  let current = start;
+
+  while (true) {
+    yield prefix + '-' + current;
+    current += 1;
+  }
 }
