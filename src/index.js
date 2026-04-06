0c54d71a5013159303212b645c999eae3cdbbedf
Denis Svirko <denis.svirko@users.noreply.github.com>
2026-04-06 18:27:00 +0300

    Add lesson for topic 19 exercise 02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-export function hello() {
-  return 'js_lessons baseline';
+'use strict';
+
+export function syncTodoItemUI(itemNode, todo) {
+  const statusNode = itemNode.querySelector('[data-role="status"]');
+  const button = itemNode.querySelector('[data-action="toggle"]');
+
+  // TODO: reflect todo.done in DOM
+  void statusNode;
+  void button;
+}
+
+export function attachTodoDelegation(listNode, todos) {
+  // TODO: add one click listener to the list
+  // hint: use event.target.closest('[data-action="toggle"]')
+  void listNode;
+  void todos;
+}
+
+export function buildTodoSnapshot(listNode, todos, clickedIds) {
+  attachTodoDelegation(listNode, todos);
+
+  for (const id of clickedIds) {
+    const button = listNode.querySelector(`[data-item-id="${id}"] [data-action="toggle"]`);
+    button.dispatchEvent({ type: 'click', bubbles: true });
+  }
+
+  return {
+    doneIds: todos.filter(todo => todo.done).map(todo => todo.id),
+    pendingIds: todos.filter(todo => !todo.done).map(todo => todo.id),
+    doneCount: todos.filter(todo => todo.done).length,
+  };
 }
