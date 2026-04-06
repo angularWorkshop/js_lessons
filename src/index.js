1b847e3aa8dbcf3f4563642abe355e20ad2d6b7e
Denis Svirko <denis.svirko@users.noreply.github.com>
2026-04-06 22:53:46 +0300

    feat: add notification inheritance solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-export function hello() {
-  return 'js_lessons baseline';
+'use strict';
+
+export class Widget {
+  constructor(id) {
+    this.id = id;
+    this.isMounted = false;
+  }
+
+  mount() {
+    this.isMounted = true;
+  }
+
+  snapshot() {
+    return {
+      id: this.id,
+      isMounted: this.isMounted,
+    };
+  }
+}
+
+export class NotificationWidget extends Widget {
+  constructor(id, message) {
+    super(id);
+    this.message = message;
+    this.isRead = false;
+  }
+
+  markAsRead() {
+    this.isRead = true;
+  }
+
+  snapshot() {
+    return {
+      ...super.snapshot(),
+      message: this.message,
+      isRead: this.isRead,
+    };
+  }
 }
